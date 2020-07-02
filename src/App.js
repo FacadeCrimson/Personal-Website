5ddf68a3345306840cf314027051503821d3186d
Lechen Tan <tan.lechen@gmail.com>
2020-07-02 11:54:57 -0400

    feat: implement response form

--- src/App.js
+++ src/App.js
@@ -65,6 +65,7 @@ const container3 = <Container title="Hello !" img={image5} color="#f6eeea"/>;
 const container4 = <Container title="Japanese" desc="I love Japanese culture very much and even passed Japanese JLPT N1 test. I am intrigued by design, traveling, films, writing, art, armchair philosophy, fabulous food, and even better conversations.
 Seeking to be inspired, to envision the unlikely, to work hard for things that are worth it, and to be surrounded by those who bring out the best in me." color="#eeeaf6" />;
 
+
 class Main1 extends React.Component{
   constructor(props) {
     super(props);
@@ -143,9 +144,8 @@ class Main1 extends React.Component{
     return <div id = "main1" onMouseEnter={()=>this.props.onMouseEnter()} onMouseLeave={()=>this.props.onMouseLeave()} style={this.props.style1}>
       <div id="leftarrow" style={this.props.styleText}  onClick={()=>this.handleLClick()}>&#8678;</div>
       <div id="rightarrow" style={this.props.styleText} onClick={()=>this.handleRClick()}>&#8680;</div>
-      <span className = "mainlabel" >About me</span>
-      <span id="hint2" style={this.props.styleText}>⤹ You could scroll this by mouse, trackpad or touch! ⤵</span>
-      <span className = "hint" style={this.props.styleText} onClick={()=>this.props.onClick(1)}>&#10534;</span>
+      <div className="header"><span className = "mainlabel" >About me</span><span className = "hint" style={this.props.styleText} onClick={()=>this.props.onClick(1)}>&#10534;</span></div>
+      <span className="hint2" style={this.props.styleText}>⤹ You could scroll this by mouse, trackpad or touch! ⤵</span>
       <div className = "maincontent" id="m1" style={this.props.styleText} onMouseDown={(e)=>this.add(e)} onMouseUp={(e)=>this.remove(e)} onTouchStart={(e)=>this.tadd(e)} onTouchEnd={(e)=>this.tremove(e)}>
           {container1}
           {container2}
@@ -234,8 +234,11 @@ class Main2 extends React.Component{
     return <div id = "main2" onMouseEnter={()=>this.props.onMouseEnter()} onMouseLeave={()=>this.props.onMouseLeave()} style={this.props.style2}>
       <div id="leftarrow" style={this.props.styleText}  onClick={()=>this.handleLClick()}>&#8678;</div>
       <div id="rightarrow" style={this.props.styleText} onClick={()=>this.handleRClick()}>&#8680;</div>
-      <span className = "mainlabel" >Experience</span>
-      <span className = "hint" style={this.props.styleText} onClick={()=>this.props.onClick(2)}>&#10534;</span>
+      <div className="header">
+        <span className = "mainlabel" >Experience</span>
+        <span className = "hint" style={this.props.styleText} onClick={()=>this.props.onClick(1)}>&#10534;</span>
+      </div>
+      <span className="hint2" style={this.props.styleText}>⤹ You could scroll this by mouse, trackpad or touch! ⤵</span>
       <div className = "maincontent" id="m2" style={this.props.styleText} onMouseDown={(e)=>this.add(e)} onMouseUp={(e)=>this.remove(e)} onTouchStart={(e)=>this.tadd(e)} onTouchEnd={(e)=>this.tremove(e)}>
           {container1}
           {container2}
@@ -248,8 +251,11 @@ class Main2 extends React.Component{
 class Main3 extends React.Component{
   render(){
     return <div id = "main3" onMouseEnter={()=>this.props.onMouseEnter()} onMouseLeave={()=>this.props.onMouseLeave()} style={this.props.style3}>
-      <span className = "mainlabel" >Skills</span>
-      <span className = "hint" style={this.props.styleText} onClick={()=>this.props.onClick(3)}>&#10534;</span>
+      <div className="header">
+        <span className = "mainlabel" >Skill</span>
+        <span className = "hint" style={this.props.styleText} onClick={()=>this.props.onClick(1)}>&#10534;</span>
+      </div>
+      <span className="hint2" style={this.props.styleText}>⤹ You could scroll this by mouse, trackpad or touch! ⤵</span>
       <Skills style={this.props.styleText}/>
       </div>
   }
@@ -380,8 +386,11 @@ class Main4 extends React.Component{
     return <div id = "main4" onMouseEnter={()=>this.props.onMouseEnter()} onMouseLeave={()=>this.props.onMouseLeave()} style={this.props.style4}>
       <div id="leftarrow" style={this.props.styleText}  onClick={()=>this.handleLClick()}>&#8678;</div>
       <div id="rightarrow" style={this.props.styleText} onClick={()=>this.handleRClick()}>&#8680;</div>
-      <span className = "mainlabel" >Hobby</span>
-      <span className = "hint" style={this.props.styleText} onClick={()=>this.props.onClick(4)}>&#10534;</span>
+      <div className="header">
+        <span className = "mainlabel" >Hobby</span>
+        <span className = "hint" style={this.props.styleText} onClick={()=>this.props.onClick(1)}>&#10534;</span>
+      </div>
+      <span className="hint2" style={this.props.styleText}>⤹ You could scroll this by mouse, trackpad or touch! ⤵</span>
       <div className = "maincontent" id="m4" style={this.props.styleText} onMouseDown={(e)=>this.add(e)} onMouseUp={(e)=>this.remove(e)} onTouchStart={(e)=>this.tadd(e)} onTouchEnd={(e)=>this.tremove(e)}>
           {container4}
         </div> 
@@ -427,6 +436,26 @@ class First extends React.Component{
       <Sidebar onClick={(i)=>{window.scroll({top: 1200,left: 0,behavior: 'smooth'} );this.props.onClick(i)}} style={this.menuDisplay} close={()=>this.closeSidebar()}/>
     </div>
   }
+}
+
+class Response extends React.Component{
+  render(){
+    return <div id="response">
+      <form className="form" action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSddmWyx1_aJvYSIxH7sIcquntxErMgQGCygpDT-k152qnHsWA/formResponse" method="post" onsubmit="submitted=!0" target="hidden_iframe">
+        <label>Title</label>
+          <input name="entry.2093844086" type="text" pattern=".{3,}"  required title="3 characters minimum"/>
+
+          <label>Feedback</label>
+          <textarea name="entry.315161779" cols="17" rows="5" required ></textarea>
+
+          <label>Email (Optional)</label>
+          <input name="entry.1086420941" type="email" />
+
+          <input type="submit" value="Send" />
+
+      </form>
+      </div>
+}
 }
 
 class MainBottom extends React.Component{
@@ -485,6 +514,7 @@ class MainBottom extends React.Component{
     }
     return <div id = "mainbottom">
       <div id="instruction">⤺ Hover on the tabs to open it!</div>
+      <Response></Response>
        <Main1 onMouseEnter={()=>this.handleMouseOver(1)} onMouseLeave={()=>this.handleMouseLeave(1)} onClick={(i)=>this.resetStyle(i)} style1={this.state.style1} styleText={this.state.current===1 ? this.state.styleText : {}}/>
       <Main2 onMouseEnter={()=>this.handleMouseOver(2)} onMouseLeave={()=>this.handleMouseLeave(2)} onClick={(i)=>this.resetStyle(i)} style2={this.state.style2} styleText={this.state.current===2 ? this.state.styleText : {}}/>
       <Main3 onMouseEnter={()=>this.handleMouseOver(3)} onMouseLeave={()=>this.handleMouseLeave(3)} onClick={(i)=>this.resetStyle(i)} style3={this.state.style3} styleText={this.state.current===3 ? this.state.styleText : {}}/>
